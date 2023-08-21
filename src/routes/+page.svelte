c4ef75149b4d83faf5c16e75a9566b72897bd606
Cass Deckard <Cass.Deckard@alaskaair.com>
2023-08-20 17:10:18 -0700

    Spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	// import Counter from './Counter.svelte';
+	// import welcome from '$lib/images/svelte-welcome.webp';
+	// import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
@@ -17,21 +17,21 @@
 </section>
 
 <style>
-    iframe#preview-panel {
-      border: none;
-      padding: 2%;
-      height: 512px;
-      width: 42%;
-    }
+  iframe#preview-panel {
+    border: none;
+    padding: 2%;
+    height: 512px;
+    width: 42%;
+  }
 
-    textarea#source-panel {
-      border: none;
-      margin: 2%;
-      padding: 2%;
-      box-shadow: inset 0 0 0 16px darkslategray;
-      resize: none;
-      box-sizing: border-box;
-      height: 512px;
-      width: 42%;
-    }
-  </style>
+  textarea#source-panel {
+    border: none;
+    margin: 2%;
+    padding: 2%;
+    box-shadow: inset 0 0 0 16px darkslategray;
+    resize: none;
+    box-sizing: border-box;
+    height: 512px;
+    width: 42%;
+  }
+</style>
